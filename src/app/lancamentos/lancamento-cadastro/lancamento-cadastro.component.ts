788012a7ba46bd28dacbb9d4b5cc67ff74099745
detectiverust93 <lucas.henrique0023@gmail.com>
2020-11-20 15:27:40 -0300

    Salvando lancamentos editados.

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -75,10 +75,28 @@ export class LancamentoCadastroComponent implements OnInit {
   }
 
   salvar(form: FormControl){
+    if (this.editando) {
+      this.atualizarLancamento(form);
+    } else {
+      this.adicionarLancamento(form);
+    }
+  }
+
+  adicionarLancamento(form: FormControl) {
     this.lancamentoService.cadastrar(this.lancamento)
       .then(() => {
         this.toasty.success('Lancamento cadastrado com sucesso.');
         form.reset();
+        this.lancamento = new Lancamento();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  atualizarLancamento(form: FormControl) {
+    this.lancamentoService.atualizar(this.lancamento)
+      .then((lancamento) => {
+        this.lancamento = lancamento;
+        this.toasty.success('Lancamento atualizado com sucesso.');
       })
       .catch(erro => this.errorHandler.handle(erro));
   }
